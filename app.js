34e37ac9174e8ad40e489c618568092e244e016b
Newell <znewell@una.edu>
2024-08-29 13:14:58 -0500

    created docs folder

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require('express')
 const app = express()
+app.use(express.static('./public/'))
 
 console.log('im on a node server, yo');
 
 app.get('/', function (req, res) {
-  res.send('Hello Node from Ex on local dev box')
+  res.sendFile('index.html')
 })
 
-app.listen(5000)
 
-app.use(express.static('./index.html'))
+app.listen(5000)
