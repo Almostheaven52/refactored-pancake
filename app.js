8ad1779eba7176d983b8088cc884262ad2f8e695
Zach <97544927+Almostheaven52@users.noreply.github.com>
2024-08-29 14:10:03 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,21 +1,19 @@
 const express = require('express')
 const app = express()
-
+app.set('view engine', 'ejs')
 app.use(express.static('./public/'))
 
-console.log('im on a node server, yo');
+console.log('im on a node server change that and that tanad f, yo');
 
 app.get('/', function (req, res) {
-  
-  res.sendFile('index.html')
-
+  // res.send('Hello Node from Ex on local dev box')
+  res.sendFile('index.html');
 })
 
-
 app.get('/ejs', (req,res)=>{
-
+``
   res.render('index', {
-    myServerVariable : "something from the server"
+    myServerVariable : "something from server"
   });
 
 })
