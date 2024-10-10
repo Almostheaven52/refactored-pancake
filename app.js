290765b3c86b11d4501139f580566d1ef50398ad
Almostheaven52 <znewell@una.edu>
2024-10-10 02:50:49 -0600

    working mary

--- app.js
+++ app.js
@@ -94,7 +94,7 @@ app.post('/update/:id', async (req,res)=>{
   client.connect; 
   const collection = client.db("zach-db").collection("class collection");
   let result = await collection.findOneAndUpdate( 
-  {"_id": new ObjectId(req.params.id)}, { $set: {"post": "NEW POST" } }
+  {"_id": new ObjectId(req.params.id)}, { $set: {"name": "NEW POST" } }
 )
 .then(result => {
   console.log(result); 
